14174f30d911a1cf20b63f6535adc71da41f9897
xuan1302 <xuaan.ndx@gmail.com>
2023-07-13 23:59:44 +0700

    fixbug

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -73,12 +73,3 @@
     $(".menu-responsive").removeClass("show-menu");
   });
 })(jQuery);
-
-const slider = document.querySelector(".slider input");
-const img = document.querySelector(".images .img-2");
-const dragLine = document.querySelector(".slider .drag-line");
-slider.oninput = () => {
-  let sliderVal = slider.value;
-  dragLine.style.left = sliderVal + "%";
-  img.style.width = sliderVal + "%";
-};
--- template/home.php
+++ template/home.php
@@ -446,4 +446,15 @@ $image_animation = get_field( "image_animation" );
             </section>
         </div>
     </div>
-<?php get_footer();
+<?php get_footer(); ?>
+
+<script>
+const slider = document.querySelector(".slider input");
+const img = document.querySelector(".images .img-2");
+const dragLine = document.querySelector(".slider .drag-line");
+slider.oninput = () => {
+  let sliderVal = slider.value;
+  dragLine.style.left = sliderVal + "%";
+  img.style.width = sliderVal + "%";
+};
+</script>
